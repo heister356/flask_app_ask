1e6ffc0bc7e462f7ecced70cfe78774314d7bf6e
heister356 <nandhugopan20@gmail.com>
2024-03-07 11:41:08 +0530

    First endpoint

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/getdata', methods = ['GET'])
+def data_return():
+    with open('data.json','r') as f:
+      data = json.load(f)
+      return jsonify({'message': 'successfully returned data','status_code':200},{'data':data})
+    
+      
+  
+if __name__== '__main__':
+    app.run(debug=True)
