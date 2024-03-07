b6952b954e26fb37820618b4d5c6ac83a73eaf5c
heister356 <nandhugopan20@gmail.com>
2024-03-07 12:19:43 +0530

    updated code

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, jsonify
 import json
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/getdata', methods = ['GET'])
-def data_return():
-    with open('data.json','r') as f:
+@app.route('/get_data', methods = ['GET'])
+def get_all_data():
+    with open('data.json') as f:
       data = json.load(f)
-      return jsonify({'message': 'success', 'data':data, 'status_code':200})
+    return jsonify({'message': 'success', 'data':data, 'status_code':200})
     
       
   
 if __name__== '__main__':
     app.run(debug=True)
+    
+    
